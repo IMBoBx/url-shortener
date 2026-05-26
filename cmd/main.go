14e2cb3bb0bbd309ef12c37c6c9b1790110df828
IMBoBx <garvkundnaniyt@gmail.com>
2026-05-27 01:51:09 +0530

    accept env vars

---
+++ cmd/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/IMBoBx/url-shortener/internal"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("no .env file found")
+	}
+
+	databaseUrl := os.Getenv("DATABASE_URL")
+	if databaseUrl == "" {
+		log.Fatal("DATABASE_URL env var is required")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8800"
+		log.Println("defaulting to port 8800")
+	}
+
+	ctx := context.Background()
+
+	dbpool, err := pgxpool.New(ctx, databaseUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer dbpool.Close()
+
+	server := internal.NewServer(dbpool)
+
+	log.Fatal(http.ListenAndServe(":"+port, server))
+}
